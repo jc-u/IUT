9c15016260e1626b3b6b9f62b7ce89b163ad1d8a
Jean-Christophe Urbain <jcurbain.admin@gmail.com>
2023-01-10 13:39:06 +0100

    Initialisation projet

---
+++ r410-introduction/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app__header header">
+        <h1 className="header__title">Introduction to React</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
