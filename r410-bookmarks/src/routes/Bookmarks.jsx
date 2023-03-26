d43675ed5bd8a445ff1a5889569d733cc18bac77
Jean-Christophe Urbain <jcurbain.admin@gmail.com>
2023-03-26 12:04:44 +0200

    Ajout de la navigation

--- r410-bookmarks/src/routes/Bookmarks.jsx
+++ r410-bookmarks/src/routes/Bookmarks.jsx
@@ -1,11 +1,20 @@
 import React, { useEffect, useState } from "react";
 import BookmarksList from "../components/BookmarksList";
-import { Redirect, Route, Switch, useRoute } from "wouter";
+import { Redirect, Route, Switch, useLocation, useRoute } from "wouter";
 import Loading from "../components/Loading";
 import { fetchBookmark } from "../services/api/bookmarks-api";
+import Menu from "../components/Menu";
+import {
+  faEdit,
+  faLeftLong,
+  faPlus,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
+
 
 function Bookmarks() {
 
+  const [, setLocation] = useLocation();
   const [, params] = useRoute("/bookmarks/:bookmarkId?/:edit?");
   const { bookmarkId } = params;
   const [bookmark, setBookmark] = useState();
@@ -21,20 +30,45 @@ function Bookmarks() {
     return <Redirect to="/bookmarks" />;
   }
 
+  function handleDelete() {
+    setBookmark();
+    setTimeout(() => {
+      setLocation("/bookmarks");
+    }, 1000);
+    console.log("delete");
+  }
+
 
 
   return (
     <Switch>
       <Route path="/bookmarks">
+      <Menu right={{ create: { icon: faPlus, to: "/bookmarks/create" } }} />
         <BookmarksList />
       </Route>
       <Route path="/bookmarks/create">
+      <Menu left={{ back: { icon: faLeftLong, to: "/bookmarks" } }} />
         <div>Create</div>
       </Route>
       <Route path="/bookmarks/:bookmarkId">
+      <Menu
+          left={{ back: { icon: faLeftLong, to: "/bookmarks" } }}
+          right={{
+            edit: { icon: faEdit, to: `/bookmarks/${bookmarkId}/edit` },
+          }}
+        />
       {!bookmark ? <Loading /> : <div>Detail {bookmark.name}</div>}
       </Route>
       <Route path="/bookmarks/:bookmarkId/edit">
+      <Menu
+          left={{ back: { icon: faLeftLong, to: `/bookmarks/${bookmarkId}` } }}
+          right={{
+            delete: {
+              icon: faTrash,
+              on: handleDelete,
+            },
+          }}
+        />
       {!bookmark ? <Loading /> : <div>Edit {bookmark.name}</div>}
       </Route>
       <Route>
